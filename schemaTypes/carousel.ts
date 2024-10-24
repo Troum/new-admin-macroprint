1d1b3c03d9a420fcc3a9bb57108c045615eb5944
Ruslan Belchenkov <r.belchenkov@litefinance.com>
2024-10-24 15:06:40 +0300

    Chnages

--- schemaTypes/carousel.ts
+++ schemaTypes/carousel.ts
@@ -11,8 +11,42 @@ export const carousel = defineType({
       name: 'slides',
       title: 'Слайды карусели',
       type: 'array',
-      of: [{type: 'image'}]
-    })
+      of: [{
+        type: 'object',
+        fields: [
+          defineField({
+            name: 'description',
+            title: 'Описание слайда',
+            type: 'array',
+            of: [{
+              type: 'block',
+              styles: [
+                {title: 'Normal', value: 'normal'},
+                {title: 'H1', value: 'h1'},
+                {title: 'H2', value: 'h2'},
+                {title: 'H3', value: 'h3'},
+                {title: 'H4', value: 'h4'},
+                {title: 'H5', value: 'h5'},
+                {title: 'H6', value: 'h6'},
+                {title: 'Quote', value: 'blockquote'}
+              ],
+              lists: [
+                {title: 'Bullet', value: 'bullet'},
+                {title: 'Numbered', value: 'number'}
+              ]
+            }]
+          }),
+          defineField({
+            name: 'slide',
+            title: 'Изображение',
+            type: 'image',
+            options: {
+              hotspot: true
+            }
+          }),
+        ]
+      }]
+    }),
   ],
   options: {
     singleton: true
